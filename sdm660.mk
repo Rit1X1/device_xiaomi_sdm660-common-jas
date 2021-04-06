e3bc5d23636ec0cc5985b3c3c8d75d00f440fa74
Akash98Sky <a98mondal@gmail.com>
2021-04-06 09:46:14 +0800

    sdm660-common: Build android.hardware.bluetooth.a2dp and add it to hidl entry
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- sdm660.mk
+++ sdm660.mk
@@ -131,6 +131,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# A2DP
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.a2dp@1.0-impl \
+    android.hardware.bluetooth.a2dp@1.0-service
+
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
